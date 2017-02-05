d38f4f9b9ec8939db5f2d92b7e7cc4b47bf8dc29
Uli Köhler <ulikoehler@online.de>
2017-02-05 03:04:29 +0100

    Move thinning execution to common function

--- cv_algorithms/thinning.py
+++ cv_algorithms/thinning.py
@@ -34,15 +34,8 @@ int zhang_suen_thinning(uint8_t* binary_image, size_t width, size_t height);
 ''')
 
 
-def guo_hall(img, inplace=False):
-    """
-    Perform in-place optimized Guo-Hall thinning.
-    Returns img.
-
-    Requires a binary grayscale numpy array as input.
-
-    This calls the optimized C backend from cv_algorithms.
-    """
+def __run_thinning(img, inplace, cfun):
+    """Internal common thinning function"""
     # Copy image (it'll be changed by the C code) if not allowed to modify
     if not inplace:
         img = img.copy()
@@ -54,10 +47,23 @@ def guo_hall(img, inplace=False):
     # Extract pointer to binary data
     dptr = _ffi.cast("uint8_t*", img.ctypes.data)
 
-    rc = _libcv_algorithms.guo_hall_thinning(dptr, width, height)
+    rc = cfun(dptr, width, height)
     if rc != 0:
         raise ValueError("Internal error (return code {0}) in algorithm C code".format(rc))
     return img
+
+
+def guo_hall(img, inplace=False):
+    """
+    Perform in-place optimized Guo-Hall thinning.
+    Returns img.
+
+    Requires a binary grayscale numpy array as input.
+
+    This calls the optimized C backend from cv_algorithms.
+    """
+    return __run_thinning(img, inplace, _libcv_algorithms.guo_hall_thinning)
+    
 
 
 def zhang_suen(img, inplace=False):
@@ -69,18 +75,5 @@ def zhang_suen(img, inplace=False):
 
     This calls the optimized C backend from cv_algorithms.
     """
-    # Copy image (it'll be changed by the C code) if not allowed to modify
-    if not inplace:
-        img = img.copy()
-    # Check if image seems correct
-    __check_image_grayscale_2d(img)
-    __check_image_fortran_order(img)
-    __check_image_min_wh(img, 3, 3)
-
-    # Extract pointer to binary data
-    dptr = _ffi.cast("uint8_t*" , img.ctypes.data)
-
-    rc = _libcv_algorithms.zhang_suen_thinning(dptr, width, height)
-    if rc != 0:
-        raise ValueError("Internal error (return code {0}) in algorithm C code".format(rc))
-    return img
+    
+    return __run_thinning(img, inplace, _libcv_algorithms.zhang_suen_thinning)
