c390c070d4f779a8d5b1e4bb69d17865fca8f0ec
Uli Köhler <ulikoehler@online.de>
2017-02-05 02:57:51 +0100

    Move thinning checks to common functions to dedup code

--- cv_algorithms/thinning.py
+++ cv_algorithms/thinning.py
@@ -4,10 +4,24 @@
 Thinning algorithms
 """
 import numpy as np
-import cv2
 from ._ffi import *
 
 __all__ = ["guo_hall", "zhang_suen"]
+
+def __check_image_grayscale_2d(img):
+    """Raise if the image  is not a 2D image"""
+    nd = len(img.shape)
+    if nd == 3:
+        raise ValueError("Can only use binary (i.e. grayscale) images")
+    if nd != 2:
+        raise ValueError("Image has wrong number of dimensions ({0} instead of 2)".format(nd))
+
+def __check_image_fortran_order(img):
+    """Raise if the image is not in FORTRAN memory order"""
+    # Check array memory order
+    if np.isfortran(img): # i.e. not C-ordered
+        raise ValueError("cv_algorithms thinning implementation works only on C-ordered arrays")
+
 
 _ffi.cdef('''
 int guo_hall_thinning(uint8_t* binary_image, size_t width, size_t height);
@@ -28,22 +42,15 @@ def guo_hall(img, inplace=False):
     if not inplace:
         img = img.copy()
     # Check if image seems correct
-    nd = len(img.shape)
-    if nd == 3:
-        raise ValueError("Can only use binary (i.e. grayscale) images")
-    if nd != 2:
-        raise ValueError("img has wrong number of dimensions ({0} instead of 2)".format(nd))
-
-    # Check array memory order
-    if np.isfortran(img): # i.e. not C-ordered
-        raise ValueError("Guo-Hall implementation works only on C-ordered arrays")
+    __check_image_grayscale_2d(img)
+    __check_image_fortran_order(img)
 
     # Can't perform Guo-hall on 0-2 pixel wide/high images
     height, width = img.shape
     if height < 3 or width < 3:
         raise ValueError("Guo-Hall algorithm needs an image at least 3px wide and 3px high")
     # Extract pointer to binary data
-    dptr = _ffi.cast("uint8_t*" , img.ctypes.data)
+    dptr = _ffi.cast("uint8_t*", img.ctypes.data)
 
     rc = _libcv_algorithms.guo_hall_thinning(dptr, width, height)
     if rc != 0:
@@ -64,15 +71,8 @@ def zhang_suen(img, inplace=False):
     if not inplace:
         img = img.copy()
     # Check if image seems correct
-    nd = len(img.shape)
-    if nd == 3:
-        raise ValueError("Can only use binary (i.e. grayscale) images")
-    if nd != 2:
-        raise ValueError("img has wrong number of dimensions ({0} instead of 2)".format(nd))
-
-    # Check order
-    if np.isfortran(img): # i.e. not C-ordered
-        raise ValueError("Guo-Hall implementation works only on C-ordered arrays")
+    __check_image_grayscale_2d(img)
+    __check_image_fortran_order(img)
 
     # Can't perform Guo-hall on 0-2 pixel wide/high images
     height, width = img.shape
