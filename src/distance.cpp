2578f134e5181418eb1ea2d7a1ed0d4ab502499f
Uli Köhler <uli@techoverflow.net>
2022-01-23 21:01:39 +0100

    Add CFFI_DLLEXPORT  to distance.cpp

--- src/distance.cpp
+++ src/distance.cpp
@@ -8,10 +8,10 @@
 
 //Forward declaration required due to CFFI's requirement to have unmangled symbols
 extern "C" {
-	int pairwise_diff(const double* a, const double* b, double* result, size_t awidth, size_t bwidth);
+	CFFI_DLLEXPORT int pairwise_diff(const double* a, const double* b, double* result, size_t awidth, size_t bwidth);
 }
 
-int pairwise_diff(const double* a, const double* b, double* result, size_t awidth, size_t bwidth) {
+CFFI_DLLEXPORT int pairwise_diff(const double* a, const double* b, double* result, size_t awidth, size_t bwidth) {
 
 	//Iterate over all (a,b) element pairs
 	for (size_t ax = 0; ax < awidth; ++ax) {
