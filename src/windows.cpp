143521faeef4619ff6f0257d63599f1a4bbb9ffd
Uli Köhler <uli@techoverflow.net>
2022-01-23 19:05:29 +0100

    More comments on windows.cpp

--- src/windows.cpp
+++ src/windows.cpp
@@ -1,4 +1,5 @@
 /**
  * This contains hacks to get the installation on Windows working.
+ * This fixes error LNK2001: unresolved external symbol PyInit__cv_algorithms
  */
 void PyInit__cv_algorithms(void) { }
