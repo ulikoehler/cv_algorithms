297241591872dbd7ca1ff7c842b31c5a33873d51
Uli Köhler <uli@techoverflow.net>
2022-01-23 21:06:49 +0100

    Define CFFI_DLLEXPORT in common.h

--- src/common.hpp
+++ src/common.hpp
@@ -5,3 +5,12 @@
  * Assumes that the width variable is defined to the width of the image
  */
 #define IMG_XY(img, x, y) img[(x) + (width)*(y)]
+
+// Windows compatibility
+#ifndef CFFI_DLLEXPORT
+#if defined(_MSC_VER)
+#define CFFI_DLLEXPORT  extern __declspec(dllimport)
+#else // !defined(_MSC_VER)
+#define CFFI_DLLEXPORT  extern
+#endif // defined(_MSC_VER)
+#endif // ifndef CFFI_DLLEXPORT
